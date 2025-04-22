9e0beda0b88f0639a45115020147e50f6c6ce054
Ehsan Saleem <ehsandesigner.s@gmail.com>
2025-04-23 00:59:18 +0530

    Update get_uploaded.js

--- app/hooks/get_uploaded.js
+++ app/hooks/get_uploaded.js
@@ -1,21 +1,19 @@
-'use server'
-
 import { useAuth0 } from "@auth0/auth0-react";
 
 
-    async function useData() {
-        const {user, isLoading, error} = await useAuth0()
+     function useData() {
+        const {user, isLoading, error} = useAuth0()
         let uploaded_courses = []
         let loaded = false
         const formData = new FormData()
             formData.append("id", user.email)
       
-            const res = await fetch("/api/uploaded_courses", {
+            const res = fetch("/api/uploaded_courses", {
               method: "POST",
               body: formData
             })
       
-            const parsedres = await res.json()
+            const parsedres = res.json()
       
             uploaded_courses = parsedres.result
             console.log("data has been set")
