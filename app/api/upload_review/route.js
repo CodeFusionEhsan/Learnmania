751c6a497eb9d6a13e71c4ae68c161a94097d5a4
Ehsan Saleem <ehsandesigner.s@gmail.com>
2025-04-23 00:38:31 +0530

    Update route.js

--- app/api/upload_review/route.js
+++ app/api/upload_review/route.js
@@ -13,7 +13,7 @@ export async function POST(req, Response) {
 
     if (name && review) {
 
-       await mongoose.connect("mongodb+srv://ehsan:ehsan2024@cluster0.vqrb8yl.mongodb.net/Learnmania?retryWrites=true&w=majority")
+       await mongoose.connect(process.env.MONGO_URI)
             console.log("Connected to database")
 
        const obj = {
