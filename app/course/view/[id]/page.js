d83aa33190039194510c1190f5cde24398d748c7
Ehsan Saleem <ehsandesigner.s@gmail.com>
2025-04-24 18:38:21 +0530

    Update page.js

--- app/course/view/[id]/page.js
+++ app/course/view/[id]/page.js
@@ -23,7 +23,7 @@ const CourseDetailPage = () => {
     const getCourse = async () => {
       const formData = new FormData()
       const url = window.location.href
-      const id = url.replace("http://learnmania.vercel.app/course/view/", "")
+      const id = url.replace("https://learnmania.vercel.app/course/view/", "")
       console.log(id)
       formData.append("id", id)
   
